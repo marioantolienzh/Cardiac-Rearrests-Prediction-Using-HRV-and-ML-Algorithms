4bae3918b89cb28fffd7cc63eafdbaabdb1fc65f
marioantolienzh <50441834+marioantolienzh@users.noreply.github.com>
2021-10-31 00:37:26 -0400

    Updated main.py for complete data document
    Read README.md for instructions

--- main.py
+++ main.py
@@ -1,430 +1,106 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 20 11:37:55 2021
+Created on Sat Oct 30 11:51:34 2021
 
-@author: marioantolinezherrera
+@author: marioantolinezherrer
 """
-
-# system imports
+import pyhrv as hrv
+import biosppy
+from csv import reader
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
 import os
 import sys
 
-# data science
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.patches import Ellipse
-import seaborn as sns
+filename_xlsx = '/.../data_path/EBME 421 Project - HRV Files.xlsx'
 
-# signal processing
-from scipy import signal
-from scipy.ndimage import label
-from scipy.stats import zscore
-from scipy.interpolate import interp1d
-from scipy.integrate import trapz
+#1. obtaining the data from a single .csv file
+# data = list()
+# with open(filename, "r") as fd:
+#   for line in fd:
+#       line = line.strip()
+#       if line != 'RR Intervals': #go to second line 
+#           data.append(float(line))
+          
 
-# misc
-import warnings
 
-# style settings
-sns.set(style='whitegrid', rc={'axes.facecolor': '#EFF2F7'})
+#2. obtaining the data from the .xlsx file
+df = pd.read_excel(filename_xlsx, sheet_name='Sheet1')
+columns = df.columns
+print(columns)
 
-# sample frequency for ECG sensor
-settings = {}
-settings['fs'] = 500
 
-# data recorded with the ECG sensor
-df = pd.read_csv("/Users/marioantolinezherrera/Desktop/CWRU/EBME_421/course_project/py_codes/kaggle_code/ecg.csv", sep=";", index_col="ms")
-#rr_manual = np.loadtxt("../input/manually-corrected-rrintervals/manual-correction-rr.txt", dtype=int)
+#creating dataframes for every data column 
+id_col = df['ID'] #patient id
+RR_int_col = df['RR Int'] #RR intervals data
+type_col = df['Type'] #rearrest type
 
-plt.figure(figsize=(20, 7))
-start = 0
-stop = 5000
-duration = (stop-start) / settings['fs']
-plt.title("ECG signal, slice of %.1f seconds" % duration)
-plt.plot(df[start:stop].index, df[start:stop].heartrate, color="#51A6D8", linewidth=1)
-plt.xlabel("Time (ms)", fontsize=16)
-plt.ylabel("Amplitude (arbitrary unit)")
-plt.show()
 
-def detect_peaks(ecg_signal, threshold=0.3, qrs_filter=None):
-    '''
-    Peak detection algorithm using cross corrrelation and threshold 
-    '''
-    if qrs_filter is None:
-        # create default qrs filter, which is just a part of the sine function
-        t = np.linspace(1.5 * np.pi, 3.5 * np.pi, 15)
-        qrs_filter = np.sin(t)
+#Select directory where we are going to work & remove previous files
+dir = '/.../results_folder_path/hrv_results'
+for f in os.listdir(dir):
+    os.remove(os.path.join(dir, f))
     
-    # normalize data
-    ecg_signal = (ecg_signal - ecg_signal.mean()) / ecg_signal.std()
+#This method creates creates a .csv results file from the patients_data
+#counter value is used to assign a new name to each results
+def evaluate_patient_data(input_data, counter):
+    file_creator_index = counter;
+    hrv_out = hrv.hrv(input_data)  
+    dict_out = hrv_out.as_dict()
+    csv_columns = dict_out.keys()
+    csv_file = dir + '/' + str(file_creator_index) + '.csv'
+    file = open(csv_file, 'x')
+    with open(csv_file, 'w') as f:
+        for key in dict_out.keys():
+            f.write("%s,%s\n"%(key,dict_out[key]))
+            
+            
+#reads data from each patient, appends it into a data list (patient_data)
+#and evaluates function in a loop over every element in data row
+patients_list = list()
+patient_data = list()
+restart = 1 #1 means true (means it must not calculate evaluate the function as patients_data list is empty)
+counter = 0
+previous_item = 0
+patient_data.clear()
 
-    # calculate cross correlation
-    similarity = np.correlate(ecg_signal, qrs_filter, mode="same")
-    similarity = similarity / np.max(similarity)
-
-    # return peaks (values in ms) using threshold
-    return ecg_signal[similarity > threshold].index, similarity
-
-def get_plot_ranges(start=10, end=20, n=5):
-    '''
-    Make an iterator that divides into n or n+1 ranges. 
-    - if end-start is divisible by steps, return n ranges
-    - if end-start is not divisible by steps, return n+1 ranges, where the last range is smaller and ends at n
+for i in id_col: #iterates through patient ids column
     
-    # Example:
-    >> list(get_plot_ranges())
-    >> [(0.0, 3.0), (3.0, 6.0), (6.0, 9.0)]
+    # if ((not RR_int_col.get(i)) and (not RR_int_col.get(previous_item))): #exits if two consecutive blank cells are detected (end)
+    #         print('All data has been processed :)')
+    #         sys.exit()
+            
+    if(i == previous_item): #if same patient as previous row, append RR_int_col data to patient_data
+        if(RR_int_col.get(i) != None):
+            patient_data.append(int(RR_int_col.get(i)))
+    else: #if different patient as previous row, evaluate function and clear patient_data 
+        if(restart != 1):
+            try:
+                evaluate_patient_data(patient_data, counter)
+            except ValueError:
+                    pass
+            patient_data.clear()
+            restart = 1
+        else:
+            restart = 0
+            patients_list.append(i)
+            
+    previous_item = i   
+    counter +=1
 
-    '''
-    distance = end - start
-    for i in np.arange(start, end, np.floor(distance/n)):
-        yield (int(i), int(np.minimum(end, np.floor(distance/n) + i)))
-        
-sampfrom = 60000
-sampto = 70000
-nr_plots = 1
-
-for start, stop in get_plot_ranges(sampfrom, sampto, nr_plots):
-    # get slice data of ECG data
-    cond_slice = (df.index >= start) & (df.index < stop) 
-    ecg_slice = df.heartrate[cond_slice]
-
-    # detect peaks
-    peaks, similarity = detect_peaks(ecg_slice, threshold=0.3)
+#To do:
+    #1) stop if two blank cells are detected
     
-    # plot similarity
-    plt.figure(figsize=(20, 15))
-
-    plt.subplot(211)
-    plt.title("ECG signal with found peaks")
-    plt.plot(ecg_slice.index, ecg_slice, label="ECG", color="#51A6D8", linewidth=1)
-    plt.plot(peaks, np.repeat(600, peaks.shape[0]), label="peaks", color="orange", marker="o", linestyle="None")
-    plt.legend(loc="upper right")
-    plt.xlabel("Time (milliseconds)")
-    plt.ylabel("Amplitude (arbitrary unit)")
+    #2) enter patients_list as function argument to name each result_file with 
+    #the name of the patient
     
-    plt.subplot(212)
-    plt.title('Similarity with QRS template')
-    plt.plot(ecg_slice.index, similarity, label="Similarity with QRS filter", color="olive", linewidth=1)
-    plt.legend(loc="upper right")
-    plt.xlabel("Time (milliseconds)")
-    plt.ylabel("Similarity (normalized)")
+    #3) analyse and interpretate results of arrest type with result data
     
-    
-def group_peaks(p, threshold=5):
-    '''
-    The peak detection algorithm finds multiple peaks for each QRS complex. 
-    Here we group collections of peaks that are very near (within threshold) and we take the median index 
-    '''
-    # initialize output
-    output = np.empty(0)
-
-    # label groups of sample that belong to the same peak
-    peak_groups, num_groups = label(np.diff(p) < threshold)
-
-    # iterate through groups and take the mean as peak index
-    for i in np.unique(peak_groups)[1:]:
-        peak_group = p[np.where(peak_groups == i)]
-        output = np.append(output, np.median(peak_group))
-    return output
-
-# detect peaks
-peaks, similarity = detect_peaks(df.heartrate, threshold=0.3)
-
-# group peaks
-grouped_peaks = group_peaks(peaks)
-
-# plot peaks
-plt.figure(figsize=(20, 7))
-plt.title("Group similar peaks together")
-plt.plot(df.index, df.heartrate, label="ECG", color="#51A6D8", linewidth=2)
-plt.plot(peaks, np.repeat(600, peaks.shape[0]),label="samples above threshold (found peaks)", color="orange", marker="o", linestyle="None")
-plt.plot(grouped_peaks, np.repeat(620, grouped_peaks.shape[0]), label="median of found peaks", color="k", marker="v", linestyle="None")
-plt.legend(loc="upper right")
-plt.xlabel("Time (ms)")
-plt.ylabel("Amplitude (arbitrary unit)")
-plt.gca().set_xlim(0, 200)
-plt.show()# detect peaks
-peaks, similarity = detect_peaks(df.heartrate, threshold=0.3)
-
-# group peaks
-grouped_peaks = group_peaks(peaks)
-
-# plot peaks
-plt.figure(figsize=(20, 7))
-plt.title("Group similar peaks together")
-plt.plot(df.index, df.heartrate, label="ECG", color="#51A6D8", linewidth=2)
-plt.plot(peaks, np.repeat(600, peaks.shape[0]),label="samples above threshold (found peaks)", color="orange", marker="o", linestyle="None")
-plt.plot(grouped_peaks, np.repeat(620, grouped_peaks.shape[0]), label="median of found peaks", color="k", marker="v", linestyle="None")
-plt.legend(loc="upper right")
-plt.xlabel("Time (ms)")
-plt.ylabel("Amplitude (arbitrary unit)")
-plt.gca().set_xlim(0, 200)
-plt.show()
-
-# detect peaks
-peaks, similarity = detect_peaks(df.heartrate, threshold=0.3)
-
-# group peaks so we get a single peak per beat (hopefully)
-grouped_peaks = group_peaks(peaks)
-
-# RR-intervals are the differences between successive peaks
-rr = np.diff(grouped_peaks)
-
-# plot RR-intervals
-plt.figure(figsize=(20, 7))
-plt.title("RR-intervals")
-plt.xlabel("Time (ms)")
-plt.ylabel("RR-interval (ms)")
-
-plt.plot(np.cumsum(rr), rr, label="RR-interval", color="#A651D8")
-plt.show()
-
-
-plt.figure(figsize=(20, 7))
-plt.title("Distribution of RR-intervals")
-
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore") # ignore FutureWarning 
-    sns.kdeplot(rr, label="rr-intervals", color="#A651D8", shade=True)
-
-outlier_low = np.mean(rr) - 2 * np.std(rr)
-outlier_high = np.mean(rr) + 2 * np.std(rr)
-
-plt.axvline(x=outlier_low)
-plt.axvline(x=outlier_high, label="outlier boundary")
-plt.text(outlier_low - 370, 0.004, "outliers low (< mean - 2 sigma)")
-plt.text(outlier_high + 20, 0.004, "outliers high (> mean + 2 sigma)")
-
-plt.xlabel("RR-interval (ms)")
-plt.ylabel("Density")
-
-plt.legend()
-plt.show()
-
-plt.figure(figsize=(20, 7))
-
-rr_corrected = rr.copy()
-rr_corrected[np.abs(zscore(rr)) > 2] = np.median(rr)
-
-plt.title("RR-intervals")
-plt.xlabel("Time (ms)")
-plt.ylabel("RR-interval (ms)")
-
-plt.plot(rr, color="red", label="RR-intervals")
-plt.plot(rr_corrected, color="green",  label="RR-intervals after correction")
-plt.legend()
-plt.show()
-
-sampfrom = 240000
-sampto = 250000
-nr_plots = 1
-
-# detect peaks
-peaks, similarity = detect_peaks(df.heartrate, threshold=0.3)
-
-# group peaks so we get a single peak per beat (hopefully)
-grouped_peaks = group_peaks(peaks)
-
-# RR-intervals are the differences between successive peaks
-rr = np.diff(grouped_peaks)
-
-for start, stop in get_plot_ranges(sampfrom, sampto, nr_plots):
-    # plot similarity
-    plt.figure(figsize=(20, 10))
-
-    plt.title("ECG signal & RR-intervals")
-    plt.plot(df.index, df.heartrate, label="ECG", color="#51A6D8", linewidth=1)
-    plt.plot(grouped_peaks, np.repeat(600, grouped_peaks.shape[0]), markersize=10, label="Found peaks", color="orange", marker="o", linestyle="None")
-    plt.legend(loc="upper left")
-    plt.xlabel("Time (milliseconds)", fontsize=16)
-    plt.ylabel("Amplitude (arbitrary unit)", fontsize=16)
-    plt.gca().set_ylim(400, 800)
-    
-    ax2 = plt.gca().twinx()
-    #ax2.plot(np.cumsum(rr_manual)+peaks[0], rr_manual, label="Corrected RR-intervals", fillstyle="none", color="#A651D8", markeredgewidth=1, marker="o", markersize=12)
-    ax2.plot(np.cumsum(rr)+peaks[0], rr, label="RR-intervals", color="k", linewidth=2, marker=".", markersize=8)
-    
-    ax2.set_xlim(start, stop)
-    ax2.set_ylim(-2000, 2000)
-    ax2.legend(loc="upper right")
-    plt.xlabel("Time (ms)")
-    plt.ylabel("RR-interval (ms)")
-    
-    
-    
-def timedomain(rr):
-    results = {}
-
-    hr = 60000/rr
-    
-    results['Mean RR (ms)'] = np.mean(rr)
-    results['STD RR/SDNN (ms)'] = np.std(rr)
-    results['Mean HR (Kubios\' style) (beats/min)'] = 60000/np.mean(rr)
-    results['Mean HR (beats/min)'] = np.mean(hr)
-    results['STD HR (beats/min)'] = np.std(hr)
-    results['Min HR (beats/min)'] = np.min(hr)
-    results['Max HR (beats/min)'] = np.max(hr)
-    results['RMSSD (ms)'] = np.sqrt(np.mean(np.square(np.diff(rr))))
-    results['NNxx'] = np.sum(np.abs(np.diff(rr)) > 50)*1
-    results['pNNxx (%)'] = 100 * np.sum((np.abs(np.diff(rr)) > 50)*1) / len(rr)
-    return results
-
-
-print("Time domain metrics - automatically corrected RR-intervals:")
-for k, v in timedomain(rr).items():
-    print("- %s: %.2f" % (k, v))
-
-print()
-print("Time domain metrics - manually corrected RR-intervals:")
-for k, v in timedomain(rr).items(): #changed rr_manual to rr
-    print("- %s: %.2f" % (k, v))
-    
-    
-# create interpolation function based on the rr-samples. 
-x = np.cumsum(rr) / 1000.0 #changed rr_manual to rr
-f = interp1d(x, rr, kind='cubic') #changed rr_manual to rr
-
-# sample rate for interpolation
-fs = 4.0
-steps = 1 / fs
-
-# now we can sample from interpolation function
-xx = np.arange(1, np.max(x), steps)
-rr_interpolated = f(xx)
-
-plt.figure(figsize=(20, 15))
-
-plt.subplot(211)
-plt.title("RR intervals")
-#plt.plot(x, rr_manual, color="k", markerfacecolor="#A651D8", markeredgewidth=0, marker="o", markersize=8)
-plt.xlabel("Time (s)")
-plt.ylabel("RR-interval (ms)")
-plt.title("Interpolated")
-plt.gca().set_xlim(0, 20)
-
-plt.subplot(212)
-plt.title("RR-Intervals (cubic interpolation)")
-plt.plot(xx, rr_interpolated, color="k", markerfacecolor="#51A6D8", markeredgewidth=0, marker="o", markersize=8)
-plt.gca().set_xlim(0, 20)
-plt.xlabel("Time (s)")
-plt.ylabel("RR-interval (ms)")
-plt.show()
-
-
-def frequency_domain(rri, fs=4):
-    # Estimate the spectral density using Welch's method
-    fxx, pxx = signal.welch(x=rr_interpolated, fs=fs)
-    
-    '''
-    Segement found frequencies in the bands 
-     - Very Low Frequency (VLF): 0-0.04Hz 
-     - Low Frequency (LF): 0.04-0.15Hz 
-     - High Frequency (HF): 0.15-0.4Hz
-    '''
-    cond_vlf = (fxx >= 0) & (fxx < 0.04)
-    cond_lf = (fxx >= 0.04) & (fxx < 0.15)
-    cond_hf = (fxx >= 0.15) & (fxx < 0.4)
-    
-    # calculate power in each band by integrating the spectral density 
-    vlf = trapz(pxx[cond_vlf], fxx[cond_vlf])
-    lf = trapz(pxx[cond_lf], fxx[cond_lf])
-    hf = trapz(pxx[cond_hf], fxx[cond_hf])
-    
-    # sum these up to get total power
-    total_power = vlf + lf + hf
-
-    # find which frequency has the most power in each band
-    peak_vlf = fxx[cond_vlf][np.argmax(pxx[cond_vlf])]
-    peak_lf = fxx[cond_lf][np.argmax(pxx[cond_lf])]
-    peak_hf = fxx[cond_hf][np.argmax(pxx[cond_hf])]
-
-    # fraction of lf and hf
-    lf_nu = 100 * lf / (lf + hf)
-    hf_nu = 100 * hf / (lf + hf)
-    
-    results = {}
-    results['Power VLF (ms2)'] = vlf
-    results['Power LF (ms2)'] = lf
-    results['Power HF (ms2)'] = hf   
-    results['Power Total (ms2)'] = total_power
-
-    results['LF/HF'] = (lf/hf)
-    results['Peak VLF (Hz)'] = peak_vlf
-    results['Peak LF (Hz)'] = peak_lf
-    results['Peak HF (Hz)'] = peak_hf
-
-    results['Fraction LF (nu)'] = lf_nu
-    results['Fraction HF (nu)'] = hf_nu
-    return results, fxx, pxx
-
-print("Frequency domain metrics:")
-results, fxx, pxx = frequency_domain(rr_interpolated)
-
-for k, v in results.items():
-    print("- %s: %.2f" % (k, v))
-    
-    
-plt.figure(figsize=(20, 7))
-plt.plot(fxx, pxx, color="k", linewidth=0.3)
-plt.title("FFT Spectrum (Welch's periodogram)")
-
-# create interpolation function for plotting frequency bands
-psd_f = interp1d(fxx, pxx)
-
-# setup frequency bands for plotting
-x_vlf = np.linspace(0, 0.04, 100)
-x_lf = np.linspace(0.04, 0.15, 100)
-x_hf = np.linspace(0.15, 0.4, 100)
-
-plt.gca().fill_between(x_vlf, psd_f(x_vlf), alpha=0.2, color="#A651D8", label="VLF")
-plt.gca().fill_between(x_lf, psd_f(x_lf), alpha=0.2, color="#51A6D8", label="LF")
-plt.gca().fill_between(x_hf, psd_f(x_hf), alpha=0.2, color="#D8A651", label="HF")
-
-plt.gca().set_xlim(0, 0.5)
-plt.gca().set_ylim(0)
-plt.xlabel("Frequency (Hz)")
-plt.ylabel("Density")
-plt.legend()
-plt.show()
-
-
-def plot_poincare(rr):
-    rr_n = rr[:-1]
-    rr_n1 = rr[1:]
-
-    sd1 = np.sqrt(0.5) * np.std(rr_n1 - rr_n)
-    sd2 = np.sqrt(0.5) * np.std(rr_n1 + rr_n)
-
-    m = np.mean(rr)
-    min_rr = np.min(rr)
-    max_rr = np.max(rr)
-    
-    plt.figure(figsize=(10, 10))
-    plt.title("Poincare plot")
-
-    sns.scatterplot(x=rr_n, y=rr_n1, color="#51A6D8")
-
-    plt.xlabel(r'$RR_n (ms)$')
-    plt.ylabel(r'$RR_{n+1} (ms)$')
-
-    e1 = Ellipse((m, m), 2*sd1, 2*sd2, angle=-45, linewidth=1.2, fill=False, color="k")
-    plt.gca().add_patch(e1)
-
-    plt.arrow(m, m, (max_rr-min_rr)*0.4, (max_rr-min_rr)*0.4, color="k", linewidth=0.8, head_width=5, head_length=5)
-    plt.arrow(m, m, (min_rr-max_rr)*0.4, (max_rr-min_rr)*0.4, color="k", linewidth=0.8, head_width=5, head_length=5)
-
-    plt.arrow(m, m, sd2 * np.sqrt(0.5), sd2 * np.sqrt(0.5), color="green", linewidth=5)
-    plt.arrow(m, m, -sd1 * np.sqrt(0.5), sd1 * np.sqrt(0.5), color="red", linewidth=5)
-
-    plt.text(max_rr, max_rr, "SD2", fontsize=20, color="green")
-    plt.text(m-(max_rr-min_rr)*0.4-20, max_rr, "SD1", fontsize=20, color="red")
-    
-    return sd1, sd2
-
-sd1, sd2 = plot_poincare(rr) #changed rr_manual to rr
-print("SD1: %.3f ms" % sd1)
-print("SD2: %.3f ms" % sd2)
+#References:
+    #https://pyhrv.readthedocs.io/en/latest/_pages/api/hrv.html#ref-hrvfunc 
+    #https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.html 
+    #https://biosppy.readthedocs.io/en/stable/tutorial.html
+    #https://www.tutorialspoint.com/How-to-save-a-Python-Dictionary-to-CSV-file
 
